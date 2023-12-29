235f1068850d42bfcda5192fa1f31f2f70b43245
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2023-12-29 15:48:40 +0200

    deploy fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = 'https://api.rawg.io/api';
 axios.interceptors.request.use((config) => {
   config.params = {
     ...config.params,
-    key: process.env.VUE_APP_RAWG_API_KEY,
+    key: '079507180c7846d9a39dcdcd3d3cb26c',
   };
   return config;
 });
